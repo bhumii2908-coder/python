d00ef39011305812b5ba47ea84567a942484bed6
bhumii2908-coder <bhumii2908@gmail.com>
2026-04-26 12:26:24 +0530

    practice1.py

---
+++ practice1.py
@@ -0,0 +1,11 @@
+# simple calculater
+num1=float(input("enter first num:"))
+num2=float(input("enter first num:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+print("sum:",sum)
+print("difference:",difference)
+print("product:",product)
+print("quotient:",quotient)
