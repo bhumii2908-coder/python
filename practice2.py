48652666758567412b544d3bea8f2ab2294f6c08
bhumii2908-coder <bhumii2908@gmail.com>
2026-04-28 21:02:20 +0530

    practice2.py

---
+++ practice2.py
@@ -0,0 +1,88 @@
+Write a program that asks the user to input a number and prints whether the number is positive.
+num=int(input("enter num:"))
+if num>0:
+        print("positive")
+# else:
+#      print(" ")
+
+
+
+Write a program that asks the user to input a number and prints whether the number is positiveor nagitive
+num=int(input("enter num:"))
+if num>0:
+        print("positive")
+else:
+     print(" negitive")
+
+
+num=int(input("enter num:"))
+if num % 2==0 :
+    if num %2!=0:
+                elif num>0:
+                        elif num<0:
+                else:
+                    print("positive")
+                    
+        else:
+            print("negitive")
+    else:
+        print("positive odd")
+else:
+    print("positive even")
+
+#break and continue both and pass
+
+for i in range(1,11):
+    if i == 5:
+         break# continue
+    print(i)
+  
+#   while   
+i=0   
+while i <10:
+    i=i+1
+    if i ==5:
+      break # continue 
+    print(i)
+ 
+#while 
+i = 0
+while i < 5:
+    i += 1
+    if i == 3:
+        pass
+    print(i) 
+    
+    
+#for loop 
+for i in range(1,20):
+    if i==11:
+        pass
+    print(i)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+
+    
