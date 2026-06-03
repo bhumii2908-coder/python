764cc84635b106f4e63d001b88625ac8c6696ba9
bhumii2908-coder <bhumii2908@gmail.com>
2026-06-04 00:56:18 +0530

    lecture2.py

---
+++ lecture2.py
@@ -0,0 +1,106 @@
+# variable
+# # declaring and assigning variable
+age=32
+height=5.1
+name=" lily"
+is_student=True
+print("age:",age)
+print("height:",height)
+print("name:",name)
+print("is_student:",is_student)
+
+
+# # understand variable types
+age=32
+height=5.1
+name=" lily"
+is_student=True
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+# # type checking and conversion
+age=32
+print(type(age))
+
+age_str=str(age)
+print(age_str)
+print(type(age_str))
+
+# # type conversion
+height=5.1
+print(type(height))
+
+height_str=str(height)
+print(height_str)
+print(type(height_str))
+
+# # type conversion
+name="123"
+print(type(name))
+
+name_int=int(name)
+print(name_int)
+print(type(name_int))
+
+# # type conversion
+is_student=True
+print(type(is_student))
+
+is_student_float=float(is_student)
+print(is_student_float)
+print(type(is_student_float))
+
+
+# conversion
+height=5.1
+type(height)
+int(float(height))
+print(int(float(height)))
+
+# conversion
+size=43
+type(size)
+float(int(size))
+print(float(int(size)))
+
+# daynamic typeing
+# python allow type of variable to chn=ange as a program executes
+
+value=20
+print(value,type(value))
+# change
+value="hello"
+print(value,type(value))
+# change
+value=55.777
+print(value,type(value))
+# change
+value=False
+print(value,type(value))
+
+# input
+a=str(input("what is your name:"))
+print(a)
+
+a=int(input("what is your age:"))
+print(a)
+
+a=float(input("what is your age:"))
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
