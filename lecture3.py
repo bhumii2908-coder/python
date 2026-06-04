b6dbb9f82a2ea562bf58848564b5aa9d964b690e
bhumii2908-coder <bhumii2908@gmail.com>
2026-06-05 01:25:16 +0530

    lecture3.py

---
+++ lecture3.py
@@ -0,0 +1,51 @@
+
+
+# data type
+# integer example
+age=35
+print(type(age))
+
+# floating point datatype
+size=5.22
+print(size)
+print(type(size))
+
+# string data example
+name="lily"
+print(name)
+print(type(name))
+
+# boolean datatype
+# one way
+True
+print(type(True))
+
+# second way
+"name is lily"=True
+print("name is lily")
+print(type("name is lily"))
+
+# three way
+a=10
+b=10
+a==b
+print(a==b)
+print(type(a==b))
+
+# common error
+# if do you not use str it give you error
+value="maid"+str(5)
+print(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
