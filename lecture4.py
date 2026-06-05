63001533d82d5be53b7f595a16b160c93750b85e
bhumii2908-coder <bhumii2908@gmail.com>
2026-06-06 00:42:30 +0530

    lecture4.py

---
+++ lecture4.py
@@ -0,0 +1,73 @@
+# OPERATORS
+# ARITHMETIC OPERATION
+
+a=10
+b=5
+add_value=a+b    #addition
+sub_value=a-b    #substraction
+mult_value=a*b      #multiplication
+div_value=a/b       #divison
+floor_div_value=a//b    #floor divison
+modulus_value=a%b       #modulus 
+exponent_value=a**b     #exponentation
+
+print(add_value)
+print(sub_value)
+print(mult_value)
+print(div_value)
+print(floor_div_value)
+print(modulus_value)
+print(exponent_value)
+
+
+# comparison opreators
+# ==equvaltoo
+a=10
+b=10
+a==b
+print(a==b)
+
+str1="lily"
+str2="Lily"
+str1==str2
+print(str1==str2)
+
+# not equal to !=
+str1!=str2
+print(str1!=str2)
+
+a="kitty"
+b="Kitty"
+a!=b
+print(a!=b)
+
+# greater then >
+num1=67
+num2=77
+print(num1>num2)
+
+# lessthen <
+num1=67
+num2=77
+print(num1<num2)
+
+  
+# greater then and equal to
+num1=55
+num2=50
+print(num1>=num2)
+
+
+# lessthen equal to
+num1=5
+num2=50
+print(num1<=num2)
+
+
+# logical opreator
+# and,not,or
+
+Z=10
+Y=10
+result=Z and Y
+print(result)
