97ad96c20374972fc4dbf38aa765ded62f414e45
bhumii2908-coder <bhumii2908@gmail.com>
2026-06-08 01:32:31 +0530

    lecture5.py

---
+++ lecture5.py
@@ -0,0 +1,8 @@
+Write a program that asks the user to input a number and prints whether the number is positive.
+num=int(input("enter num:"))
+if num>0:
+        print("positive")
+elif num<0:
+     print("negitive ")
+else:
+    print("zero")
