d40ba4dbd8b781cec9b095a9eeba9710d6a2021d
bhumii2908-coder <bhumii2908@gmail.com>
2026-07-16 02:20:01 +0530

    Notes2.py

---
+++ loopsnotes2.py
@@ -0,0 +1,426 @@
+# RANGE()
+
+# print(range(5))
+# print(range(10))
+
+# seq=range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+
+# seq=range(5)
+# for i in seq:
+#     print(i)
+
+# seq=range(10)
+# for i in seq:
+#     print(i)
+
+
+# range(start,stop)
+nums=range(5,10)   
+for j in nums:
+    print(j)
+
+
+# range(start,stop,step size)
+nums=range(2,20,4)
+for j in nums:
+    print(j)
+
+
+# you can print muliplication table of 2 
+
+seq=range(2,21,2)        
+for i in seq:
+    print(i)
+    
+# you can print miltiplication table of 3
+
+seq=range(3,31,3)
+for i in seq:
+    print(i)
+
+# you can print multiplication table of 8
+
+nums=range(8,81,8)
+for j in nums: 
+    print(j)
+
+
+# you can print odd numbers
+
+seq=range(2,101,2)
+for i in seq:
+    print(i)
+ 
+# you can print even number 
+
+seq=range(3,101,3)
+for i in seq:
+    print(i)
+
+
+
+# PRACTICE QUESTIONS
+ 
+#  print number from 1 to 100
+
+for i in range(1,101):
+    print(i)
+
+
+# print number from 100 to 1
+
+for j in range(100,0,-1):
+    print (j)
+
+
+# print the muliplication number of n    
+
+for x in range(15,151,15):
+    print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
