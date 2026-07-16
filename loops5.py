30d0cc5561e47df23cbb45ed1f7e8ce8dedffbab
bhumii2908-coder <bhumii2908@gmail.com>
2026-07-16 23:13:21 +0530

    Notes.py

---
+++ loops5.py
@@ -0,0 +1,148 @@
+count=1
+while count<=5:
+    print("hello")
+    count=count+1
+    
+    
+n=1
+while n<=8:
+    print("*")
+    n=n+1
+# YOU CAN PRINT ITERTOR ALSO
+i=1
+while i<=10:
+    print("programming",i)
+    i=i+1
+  
+# number from 1to 10   
+j=1
+while j<=10:
+      print(j)
+      j=j+1
+print("loop ends")    
+      
+i=10
+while i>=1:
+    print(i)
+    i=i-1
+    
+# PRACTICE   
+# PRINT NUMBER FROM 1 TO 100
+i=1
+while i<=100:
+    print(i)
+    i=i+1
+
+# PRINT NUMBER FROM 100 TO 1
+j=100
+while j>=1:
+    print(j)
+    j=j-1
+
+# print the muliplication table of a number 3
+i=1 
+while i<=10:
+    print(3*i)
+    i=i+1
+    
+#  print the muliplication table of a number 5    
+i=1
+while i<=10:
+    print(5*i)
+    i=i+1
+    
+#  print the muliplication table of a number 15    
+i=1
+while i<=10:
+    print(15*i)
+    i=i+1
+    
+#  print the muliplication table of a number n      
+ 
+n=int(input("enter number: ")) 
+i=1
+while i<=10:
+    print(n*i)
+    i=i+1
+    
+#   print the muliplication table of a number n      
+n=int(input("enter number: "))     
+j=1
+while j<=10:
+    print(n*i)
+    i=i+1
+    
+
+    
+# print the element of the following list using a loop (1,4,9,16,25,36,49,64,81,100)
+    
+num=[1,4,9,16,25,36,49,64,81,100]  
+ 
+idx=0
+while idx<len(num):
+    print(num[idx])
+    idx=idx+1
+    
+heroes=["ironman","thor","batman","captainamerica","rock"]    
+    
+i=0
+whi le i<len(heroes):
+      print( heroes [i])
+      i=i+1
+    
+# search for a number x inthis tuple using loop
+# (1,4,3,5,45,67,23,5,67,78,3,12,24)
+
+num=(1,4,3,5,45,67,23,5,67,78,3,12,24)
+
+x=3
+i=0
+while i<len(num):
+    if(num[i]==x):
+        print("FOUND at idx",i)
+    else:
+        print("finding...")
+    i=i+1
+    
+    
+# name=("meera","tina","anjali","maya","neha","lily","tara","megha","tina","jeera","sonu","monu")    
+    
+x="tina"
+i=0
+while i< len(name):
+    if(name[i]==x):
+        print("FOUND at i",i)
+    else:
+        print("finding..")
+    i=i+1   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    
+    
+    
+    
+       
+    
+    
+    
+    
+    
+    
