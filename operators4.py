e1e2ec4cb39fb385931e2c57e78bcb9f0a3425e4
bhumii2908-coder <bhumii2908@gmail.com>
2026-07-23 23:00:05 +0530

    Practice_Notes2.py

---
+++ operators4.py
@@ -0,0 +1,240 @@
+# printSum
+a=7156
+b=3
+sum=a + b
+print(sum)
+
+a=765
+b=324
+diff=a-b
+print(diff)
+
+# Arithmetic opreators
+a=3
+b=4
+sum=a+b
+print(sum)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)# remainder 
+print(a**b)# a^b
+
+# Relational opreators
+a=50
+b=30
+print(a == b)
+print(a !=b )
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# assigment opreators
+
+num=30
+num+=10
+print("num:",num)
+
+num=40
+num-=10
+print("num:",num)
+
+num=6
+num*=5
+print("num:",num)
+
+num=8
+num/=3
+print("num:"num)
+
+num=30
+num%=6
+print("num:"num)
+
+num=40
+num**=8
+print("num:"num)
+
+# logical operators
+a=40
+b=70
+print(not True)
+print(not False)
+print(not(a<b))
+print(not(a!=b))
+print(not(a==b))
+# AND
+val1=True
+val2=False
+print("and operator:",val1 and val2)
+
+val1=True
+val2=True
+print("and operator :",val1 and val2)
+
+val1=False
+val2=True
+print("and operator:",val1 and val2)
+
+# OR
+valA=True
+valB=True
+print("OR opreator:",valA or valB)
+
+valA=True 
+valB=False
+print("OR opreator:",valA or valB)
+
+a=40
+b=70
+print("OR opreator:",(a != b) or (a == b))
+
+a=40
+b=70
+print("OR opreator:",(a<b) or (a>b))
+
+
+# type conversion
+a=2
+b=4.52
+sum=a+b		 # 2.0 + 4.52 =>6.52
+print(sum) 
+
+a="4"
+b=54
+print(type(a))
+# int
+a=int("4")
+b=4.52
+print(type(a))
+print(a+b)
+# int and flaot
+a=int("3")
+b=float("6")
+c=4.53
+print(type(a))
+print(type(b))
+print(a+b+c)
+# string
+a=3.78
+a=str(a)
+print(type(a))
+
+# input
+val=input("enter some value:")
+ print(type(val),val)    #"26","67.99"
+
+ name=input("enter your name:")
+print("welcome",name)
+ 
+name=input("enter your age:")
+print("you entered",name)
+
+name=input("enter name")
+age=int(input("enter age:"))
+marks=float(input("entermarks:"))
+print("welcome",name)
+print("age=",age)
+print("marks=",marks)
+
+#  write a program to input 2number &print their sum
+first = int(input("enter first num"))
+second = int(input("enter second num"))
+print("sum =",first + second)
+
+# WAP to print side of a squre & print its area
+side = float(input("enter square side:"))
+print("area =",side ** 2 )
+
+# WAP to input 2 floating point numbers & print their average
+a =float(input("enter first"))
+b=float(input("enter second"))
+print("avg=",(a+b)/2)
+
+# WAp to input 2 int number a and b .print true if a is greater than or equal to b.if not print false
+a= int(input("enter first"))
+b=int(input("enter second"))
+print(a >= b)
+
+# write a program to add two number as input enterd by the usre
+a= int(input("enter first"))
+b= int(input("enter second"))
+print(a/b)
+
+# write a progam to find the square and cube ofa number
+a = int(input(" enter first"))
+sqaure = a*a
+cube = a*a*a
+print("sqaure=",sqaure)
+print("cube=",cube)
+
+# find avg.5 numbers
+a=int(input("enter first"))
+b =int(input("enter second"))
+c=float(input("enter third"))
+d=float(input ("enter four"))
+e=int(input("enter five"))
+print("avg=",(a+b+c+d+e)/2)
+
+weite a program to find a area of rectangle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
